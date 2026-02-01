1dabf65b77c9515fd52f361e2b779a8cb636060f
VIGNESH <vignesh3269s@gmail.com>
2026-02-01 22:49:31 +0530

    Update server configuration to use PORT from environment variables

--- server/server.js
+++ server/server.js
@@ -101,7 +101,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = 8000;
+const PORT = process.env.PORT;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
