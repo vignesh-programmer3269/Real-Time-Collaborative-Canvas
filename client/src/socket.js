0401fb4564d7a8877de6d3e4f3e0905f95584833
VIGNESH <vignesh3269s@gmail.com>
2026-02-01 23:36:39 +0530

    Fix backend URL environment variable reference in socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = process.env.BACKEND_URL;
+const URL = process.env.REACT_APP_BACKEND_URL;
 console.log("Connecting to backend URL:", URL);
 
 export const socket = io(URL, {
