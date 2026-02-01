458b000c7120f49ae3a3a9614129896a8f314825
VIGNESH <vignesh3269s@gmail.com>
2026-02-02 00:43:01 +0530

    Remove transports option from socket.io client configuration

--- client/src/socket.js
+++ client/src/socket.js
@@ -4,6 +4,5 @@ const URL = process.env.REACT_APP_BACKEND_URL;
 console.log("Connecting to backend URL:", URL);
 
 export const socket = io(URL, {
-  transports: ["websocket"],
   autoConnect: false,
 });
