b442ba2745d8cd6e3ee0442aa0d3f56bea3cb6b0
VIGNESH <vignesh3269s@gmail.com>
2026-02-01 22:58:49 +0530

    Add console log for backend URL connection in socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
 const URL = process.env.BACKEND_URL;
+console.log("Connecting to backend URL:", URL);
 
 export const socket = io(URL, {
   autoConnect: false,
